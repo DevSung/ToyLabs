7b18683a58e32d3f138da19e12f3f1bc1e4c3daf
조성신 <sungsin1030@gmail.com>
2025-01-05 22:30:53 +0900

    Initialize project with Spring Boot and Kotlin setup

---
+++ src/main/kotlin/com/example/toylabs/ToyLabsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.toylabs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToyLabsApplication
+
+fun main(args: Array<String>) {
+    runApplication<ToyLabsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/toylabs/ToyLabsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.toylabs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToyLabsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
